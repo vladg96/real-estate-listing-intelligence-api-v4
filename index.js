feb03547dbe05fde43ba3d8ec69f1497a5efb2b2
VladG <60361148+vladg96@users.noreply.github.com>
2025-05-23 17:54:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 10000;
